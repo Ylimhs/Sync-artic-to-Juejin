e4375342e5723277ebba1015080d979409f87515
Ylimhs <i@ylimhs.cn>
2023-03-29 21:34:29 +0800

    （feat）: update action and doc
    Signed-off-by: Ylimhs <i@ylimhs.cn>

--- config/config.py
+++ config/config.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+def str_to_bool(str):
+    return True if str.lower() == 'true' else False
 # load from env
 # JueJin Config
 JUEJIN_COOKIE = os.getenv("JUEJIN_COOKIE")
@@ -14,7 +16,8 @@ CSND_ARTIC_STATUS = os.getenv("CSND_ARTIC_STATUS")
 
 if SYNC_TO_DRAFT is None or SYNC_TO_DRAFT == "":
     SYNC_TO_DRAFT = True
-
+else:
+    SYNC_TO_DRAFT = str_to_bool(SYNC_TO_DRAFT)
 if CSND_ARTIC_STATUS is None:
     CSND_ARTIC_STATUS = "enable"
 
