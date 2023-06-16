015e96919fb9a4bd6bddb752331085a48b6ff273
Ylimhs <i@ylimhs.cn>
2023-06-16 10:14:23 +0800

    (feat) add zhihuzhuanlan2jj-add time
    Signed-off-by: Ylimhs <i@ylimhs.cn>

--- convert/zhuHu2JJ.py
+++ convert/zhuHu2JJ.py
@@ -8,14 +8,23 @@ from utils.const import failed_sysc_artic_list, success_sysc_artic_dict, success
 from utils.utils import logging, html_replace_image_links
 
 
-def get_zhihu_zhuanlan_artics_info(zh_client, column_id):
+
+def timeTOS(t):
+    # 转换成时间数组
+    timeArray = time.strptime(t, "%Y-%m-%d %H:%M:%S")
+    # 转换成时间戳
+    timestamp = time.mktime(timeArray)
+    return timestamp
+
+def get_zhihu_zhuanlan_artics_info(zh_client, column_id,start_time, end_time):
     artic_info_list = list()
     # 获取总的文章数量
     res = zh_client.get_cl_artc_list(column_id)
     totals = res["paging"]["totals"]
     totalPage = totals // 100 + 1  # 获取总页数
-    totalPage = 1
-    for i in range(totalPage):
+    stop_get = False
+    # totalPage = 1
+    for i in range(totalPage and not stop_get):
         respage = zh_client.get_cl_artc_list(column_id, offset=i * 100)
         data = respage['data']
         for article in data:
@@ -24,13 +33,22 @@ def get_zhihu_zhuanlan_artics_info(zh_client, column_id):
             id = article["id"]
             excerpt = article["excerpt"]
             content = article["content"]
+            created = article['created']
             articleinfo.update({
                 "title": title,
                 "id": id,
                 "desc": excerpt,
                 "content": content
             })
-            artic_info_list.append(articleinfo)
+            if start_time is None and end_time is None:
+                artic_info_list.append(articleinfo)
+            elif start_time is not None and end_time is not None:
+                if start_time <= created <= end_time:
+                    artic_info_list.append(articleinfo)
+                elif created < start_time:
+                    stop_get = True
+                    break
+
     return artic_info_list
 
 
@@ -139,12 +157,18 @@ def publish_zhihu_to_jj(client, zhihu_zhuanlan_artics_info):
 
 def zhihu2JJ():
     logging.info("begin to sycn zhihu zhuanlan artic to jj")
+    # ZHIHU_COOKIE 可以不需要
     zh_client = zhiHuClient(ZHIHU_COOKIE)
     jj_client = JJClient(JUEJIN_COOKIE)
+    start_time = "2023-06-15 00:00:00"
+    end_time = "2023-06-15 22:59:00"
+    zhuanlanName = "qbitai"
     # zhihu_zhuanlan_artics_info = get_zhihu_zhuanlan_artics_info(zh_client, "jiqizhixin")
-    zhihu_zhuanlan_artics_info = get_zhihu_zhuanlan_artics_info(zh_client, "qbitai")
+    zhihu_zhuanlan_artics_info = get_zhihu_zhuanlan_artics_info(zh_client, zhuanlanName, timeTOS(start_time), timeTOS(end_time))
+    print(zhihu_zhuanlan_artics_info)
+    print(len(zhihu_zhuanlan_artics_info))
     publish_zhihu_to_jj(jj_client, zhihu_zhuanlan_artics_info)
-    print_result()
+    # print_result()
 
 if __name__ == '__main__':
     zhihu2JJ()
